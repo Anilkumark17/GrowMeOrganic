841cd4c4a6f4f562257d23f8288b2a8ba09685c6
Anil kumar <anilkumarkondeboina@gmail.com>
2024-01-16 20:29:56 +0530

    all issues are solved

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // Set your desired limit in kilobytes (KB)
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000, 
   },
 })
